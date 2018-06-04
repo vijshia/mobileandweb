cfc01ff696ed91131af7e3128c413a146ad33f2c
Vijay Sivasubramanian <vijshia@gmail.com>
2018-06-04 23:28:25 +0530

    addting screen for mobilesitesurvey

---
+++ src/main/java/com/kone/app/screens/konesitesurvey/LoginScreen.java
@@ -0,0 +1,8 @@
+package com.kone.app.screens.konesitesurvey;
+
+import com.kone.app.screens.WebBaseScreen;
+import static com.kone.app.screens.salesforce.LoginScreen.wdriver;
+
+public class LoginScreen extends WebBaseScreen{
+
+}
