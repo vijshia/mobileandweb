a455381ab3ad95e51ed1986a3fadbe52783cdb96
Vijay Sivasubramanian <vijshia@gmail.com>
2018-06-11 17:50:26 +0530

    opportunity will be reading from excel file

--- src/main/java/com/kone/app/screens/salesforce/MainScreen.java
+++ src/main/java/com/kone/app/screens/salesforce/MainScreen.java
@@ -3,12 +3,13 @@ package com.kone.app.screens.salesforce;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-
 import com.kone.app.screens.WebBaseScreen;
 import com.kone.app.screens.konesitesurvey.SiteLoginScreen;
 import com.kone.framework.context.WebContext;
-
 import ru.yandex.qatools.allure.annotations.Step;
+import com.kone.framework.utility.ExcelReader;
+import static com.kone.app.screens.konesitesurvey.SiteLoginScreen.excelPath;
+import static com.kone.app.screens.konesitesurvey.SiteLoginScreen.excelData;
 
 public class MainScreen extends WebBaseScreen {
 	
@@ -18,13 +19,25 @@ public class MainScreen extends WebBaseScreen {
 	private By txt_Homesearchbox=By.id("phSearchInput");
 	private By btn_Homesearchbox=By.id("phSearchButton");
 	
+	private String SF_Opportunity_Name;
+	
 	public MainScreen() {
 		wdriver = WebContext.wdriver;
 	}
 	
 	@Step("Search for Existing Opportunity")
 	public SelectOpportunityScreen searchOpportunity() {
-		enteringValueinTextField(txt_Homesearchbox, "Atest_06062018");
+		
+		ExcelReader excelReader=new ExcelReader(excelPath);
+		try {
+			excelData=excelReader.GetData("Web_Login_Data");
+			
+			SF_Opportunity_Name=excelData.get("SF_Opportunity_Name").get(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		enteringValueinTextField(txt_Homesearchbox, SF_Opportunity_Name);
 		clickonButton(btn_Homesearchbox);
 		
 		SelectOpportunityScreen selectOpportunityScreen= new SelectOpportunityScreen();
