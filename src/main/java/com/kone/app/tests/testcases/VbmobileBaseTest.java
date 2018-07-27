729da66357df2faeafb24acea9342e7b2d6bacaf
Vijay Sivasubramanian <vijshia@gmail.com>
2018-07-27 14:02:41 +0530

    included driver.context 'WEBVIEW_com.kone.vbmobile.debug' in @aftertest in vbmobile

--- src/main/java/com/kone/app/tests/testcases/VbmobileBaseTest.java
+++ src/main/java/com/kone/app/tests/testcases/VbmobileBaseTest.java
@@ -4,20 +4,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
-
 import com.kone.app.pages.vbmobile.SideMenuPage;
 import com.kone.app.pages.vbmobile.SurveyManagerPage;
 import com.kone.framework.appium.AppiumServer;
 import com.kone.framework.context.TestContext;
 import com.kone.framework.utility.Log;
-
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import ru.yandex.qatools.allure.annotations.Step;
@@ -28,8 +22,6 @@ public class VbmobileBaseTest {
 	protected static AndroidDriver<MobileElement> driver;
 	
 	private static Properties appData;
-	private By menuButton = By.xpath("//*[@aria-label='Menu']");
-	private By logoutMenu = By.xpath("//*[text()='Logout']");
 	
 	static {
 		
@@ -62,12 +54,18 @@ public class VbmobileBaseTest {
 	@AfterTest(alwaysRun = true)
 	public void logoutApplication() {
 		
+		Log.info("");
+		Log.info("**************************** AfterTest Execution has been Started ****************************");
 		SurveyManagerPage surveyManagerScreen = new SurveyManagerPage();
 		surveyManagerScreen.openSideMenu("Logout");
 
 		SideMenuPage sideMenupage = new SideMenuPage();
 		sideMenupage.logout();
-		Log.info("*********@AfterTest Completed*********");
+
+		driver.resetApp();
+		driver.context("WEBVIEW_com.kone.vbmobile.debug");
+		Log.info("**************************** AfterTest Execution has been Completed ****************************");
+		Log.info("");
 	}
 	
 	@Step("Waiting for {0} millisecond(s)")
